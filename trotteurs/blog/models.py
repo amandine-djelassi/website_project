4dae15b31963e074704c93ce8eb5a70a4733f12e
amandine-djelassi <amandine.djelassi@gmail.com>
2017-03-03 17:58:17 +0000

    Creation of the article model, and the views associated

--- trotteurs/blog/models.py
+++ trotteurs/blog/models.py
@@ -2,4 +2,30 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Tag(models.Model):
+    """
+    """
+    tag_titlle = models.CharField("Titre", max_length=200)
+
+    def __str__(self):
+        return self.tag_title
+
+class Article(models.Model):
+    """
+        An article is composed of : 
+            * a title 
+            * an image (facultative)
+            * a list of tags (facultative)
+            * a publication date 
+            * an abstract (facultative)
+            * a text
+    """
+    title = models.CharField("Titre", max_length=200)
+    # image 
+    # tags = models.ManyToManyFiled("Tag", Tag, blank=True)
+    pub_date = models.DateTimeField("Date de publication")
+    abstract = models.CharField("Résumé", max_length=200)
+    text = models.TextField("Corps de l'article")
+
+    def __str__(self):
+        return self.title
--- trotteurs/blog/views.py
+++ trotteurs/blog/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.views import generic
 
-# Create your views here.
+from .models import Article
+
+class IndexView(generic.ListView):
+    template_name = 'blog/index.html'
+    context_object_name = 'latest_article_list'
+
+    def get_queryset(self):
+        """ 
+            Return the last five published articles 
+        """
+        return Article.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    """
+    """
+    model = Article 
+    template_name = 'blog/detail.html'
