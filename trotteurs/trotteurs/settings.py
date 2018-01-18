c9e53ad5ec83acff0be7e61402c8de1dcdde21dd
amandine-djelassi <amandine.djelassi@gmail.com>
2018-01-18 10:51:18 +0100

    fix debug=true problem

--- trotteurs/trotteurs/settings.py
+++ trotteurs/trotteurs/settings.py
@@ -34,7 +34,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = get_env_variable('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = get_env_variable('DEBUG_TROTTEUR')
+DEBUG = (get_env_variable('DEBUG_TROTTEUR')=='True')
 
 # configure email
 EMAIL_USE_TLS = True
